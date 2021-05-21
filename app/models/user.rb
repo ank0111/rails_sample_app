c48a7505c7514c5e4322fe3af2eea300e9279e09
Ryohei Sakamoto <la.sose.thky@gmail.com>
2021-05-22 07:29:55 +0900

    Implement basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ before_save { email.downcase! }
                     uniqueness: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
